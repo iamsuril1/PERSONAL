32b38ee2c5ac2cfc2059fcb7dee72f72fcf80df4
iamsuril1 <129651648+iamsuril1@users.noreply.github.com>
2023-04-06 02:17:42 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openPopup() {
+    document.getElementById("samsung").style.display = "flex";
+  }
+  
+  function closePopup() {
+    document.getElementById("samsung").style.display = "none";
+  }
